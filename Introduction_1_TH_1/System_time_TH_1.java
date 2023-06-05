0b326c100c2c1e96e8d4ad9c999d70aea7b6e87c
khanhnguyen2001 <nqkhanh167@gmail.com>
2023-06-05 18:39:37 +0700

    Up Introduction_1_TH_1 folder

---
+++ Introduction_1_TH_1/System_time_TH_1.java
@@ -0,0 +1,11 @@
+package Introduction_1_TH_1;
+
+import java.util.Date;
+
+public class System_time_TH_1 {
+    public static void main(String[] args) {
+        Date timeNow = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Time now is: " + timeNow);
+    }
+}
