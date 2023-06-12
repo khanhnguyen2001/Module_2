75621d793aa9487a0b1019633a1e28f4fca9ca40
khanhnguyen2001 <nqkhanh167@gmail.com>
2023-06-12 08:23:52 +0700

    Up Demo folder

--- Demo/Circle.java
+++ Demo/Circle.java
@@ -1,2 +1,40 @@
-package Demo;public class Circle {
+package Demo;
+
+public class Circle {
+    private static int count = 0;
+    private int id;
+    private double radius;
+    private static final double PI = 3.14;
+    private static double maxRadius = 0;
+    private static double totalArea = 0;
+
+    public Circle(double radius) {
+        this.id = ++count;
+        this.radius = radius;
+        if (radius > maxRadius) {
+            maxRadius = radius;
+        }
+        totalArea += calculateArea();
+    }
+
+    public static int getNumberOfCircles() {
+        return count;
+    }
+
+    public double calculateArea() {
+        return PI * radius * radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * PI * radius;
+    }
+
+    public static double getMaxRadius() {
+        return maxRadius;
+    }
+
+    public static double getTotalArea() {
+        return totalArea;
+    }
 }
+
--- Demo/Main.java
+++ Demo/Main.java
@@ -1,2 +1,25 @@
-package Demo;public class Main {
+package Demo;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(2.5);
+        Circle circle2 = new Circle(3.0);
+        Circle circle3 = new Circle(1.8);
+
+        System.out.println("Số vòng kết nối được tạo: " + Circle.getNumberOfCircles());
+
+        System.out.println("Diện tích hình tròn 1: " + circle1.calculateArea());
+        System.out.println("Chu vi hình tròn 1: " + circle1.calculatePerimeter());
+
+        System.out.println("Diện tích hình tròn 2: " + circle2.calculateArea());
+        System.out.println("Chu vi hình tròn 2: " + circle2.calculatePerimeter());
+
+        System.out.println("Diện tích hình tròn 3: " + circle3.calculateArea());
+        System.out.println("Chu vi hình tròn 3: " + circle3.calculatePerimeter());
+
+        System.out.println("Bán kính tối đa giữa các vòng tròn: " + Circle.getMaxRadius());
+
+        System.out.println("Tổng diện tích hình tròn: " + Circle.getTotalArea());
+    }
 }
+
