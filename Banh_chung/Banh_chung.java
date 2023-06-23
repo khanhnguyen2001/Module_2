58bc301f7845389c0cb30069460a1b919605374a
khanhnguyen2001 <nqkhanh167@gmail.com>
2023-06-23 14:54:05 +0700

    Update file

--- Banh_chung/Banh_chung.java
+++ Banh_chung/Banh_chung.java
@@ -1,29 +1,4 @@
 package Banh_chung;//import java.util.Scanner;
-//
-//public class Banh_chung.Banh_chung {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int[] d = new int[5];
-//        int N = scanner.nextInt();
-//        scanner.nextLine(); // Đọc ký tự newline sau khi đọc số N
-//        for (String num : scanner.nextLine().split(" ")) {
-//            int x = Integer.parseInt(num);
-//            d[x] = d[x] + 1;
-//        }
-//
-//        int res = d[4] + d[3] + d[2] / 2;
-//        d[1] -= d[3];
-//        if (d[2] % 2 > 0) {
-//            res++;
-//            d[1] -= 2;
-//        }
-//        if (d[1] > 0) {
-//            res += (d[1] + 3) / 4;
-//        }
-//
-//        System.out.println(res);
-//    }
-//}
 
 import java.util.Scanner;
 
@@ -39,7 +14,6 @@ public class Banh_chung {
         int minCakes = calculateMinCakes(N, groupSizes);
         System.out.println(minCakes);
     }
-
     private static int calculateMinCakes(int N, int[] groupSizes) {
         int[] count = new int[5]; // Số lượng nhóm có từ 0 đến 4 thành viên
 
