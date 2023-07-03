8cdf1a95aad1089cdc218fa729b4e2ec5e165235
khanhnguyen2001 <nqkhanh167@gmail.com>
2023-07-03 14:42:00 +0700

    Up folder

---
+++ _19_String_and_Regex/TH_1_Validate_email/EmailExample.java
@@ -0,0 +1,2 @@
+package _19_String_and_Regex.TH_1_Validate_email;public class EmailExample {
+}
---
+++ _19_String_and_Regex/TH_1_Validate_email/EmailExampleTest.java
@@ -0,0 +1,4 @@
+package _19_String_and_Regex.TH_1_Validate_email;
+
+public class EmailExampleTest {
+}
